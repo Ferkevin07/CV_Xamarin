03a3fc884a7533d579334275a00a5e207b0c4e67
Ferkevin07 <ferkevin71997@hotmail.com>
2022-08-01 17:12:01 -0500

    Agregar archivos de proyecto.

---
+++ App_CV/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App_CV;
+
+namespace App_CV
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCV1_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageCV());
+        }
+    }
+}
